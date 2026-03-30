5a73e4ed1c7216d3804efde8fb88125eb15e9d9a
Michlich <150546066+Michlich@users.noreply.github.com>
2026-03-31 01:38:12 +0300

    Add files via upload

---
+++ cicd/package.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+VERSION="1.0.0"
+PACKAGE_NAME="programm"
+
+mkdir -p build/$PACKAGE_NAME-$VERSION
+cp -r DEBIAN usr build/$PACKAGE_NAME-$VERSION/
+
+dpkg-deb --build build/$PACKAGE_NAME-$VERSION
+
+mv build/$PACKAGE_NAME-$VERSION.deb .
+echo "deb created"
